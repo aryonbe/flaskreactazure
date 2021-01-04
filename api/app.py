2cc1595610f4102fb0e953c73f9e574bda32dce1
aryonbe <aryonbe@gmail.com>
2021-01-04 13:57:20 +0900

    first commit

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import time
+
+app = Flask(__name__,static_folder='../build',static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time':time.time()}
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
